153349ae0efe4821989ad57b4047110bdfe316e6
vuggy17 <vuggy17@gmail.com>
2023-11-01 17:45:42 +0700

    first commit

---
+++ apps/idle.chat/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="idle.chat" />
+    </div>
+  );
+}
+
+export default App;
