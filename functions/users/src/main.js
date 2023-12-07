dc00c817970084e6f22e836a5984ecef0dc88be6
vuggy17 <vuggy17@gmail.com>
2023-12-07 16:29:33 +0700

    update function

--- functions/users/src/main.js
+++ functions/users/src/main.js
@@ -1,21 +1,48 @@
-import { Client } from 'node-appwrite';
+import { Client, Databases, ID, Query } from 'node-appwrite';
 
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
   // Why not try the Appwrite SDK?
   //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+  log(req);
+
+  const userID = req.headers['x-appwrite-user-id'];
+  const { name, email, phone } = req.body;
+
+  const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
+
+  const database = new Databases(client);
+  const dbId = '656af2faa9d4d3352a34';
+  const collectionId = 'users';
+
+  const documents = await database.listDocuments(dbId, collectionId, [
+    Query.equal('authId', userID),
+  ]);
+  log('documents', documents);
+
+  if (!documents) {
+    const newUser = {
+      name,
+      email,
+      phone,
+      meta: name + email + phone,
+      avatar: '',
+      authId: userID,
+    };
+
+    await database.createDocument(dbId, collectionId, ID.unique());
+  } else {
+    // await database.updateDocument(dbId, collectionId, )
+    log('update user');
+  }
 
   // If something goes wrong, log an error
   error('Hello, Errors!');
-  log(req);
   // The `req` object contains the request data
   if (req.method === 'GET') {
     // Send a response with the res object helpers
