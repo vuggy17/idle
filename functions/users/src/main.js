b6be6d53d46d61d339a125ac7904132a88c17d63
vuggy17 <vuggy17@gmail.com>
2023-12-08 13:57:04 +0700

    add logs

--- functions/users/src/main.js
+++ functions/users/src/main.js
@@ -2,6 +2,9 @@ import { Client, Databases, ID, Query } from 'node-appwrite';
 
 export default async ({ req, res, log, error }) => {
   try {
+    log('Processing with: ');
+    log(req);
+
     const { name, email, phone, $id } = req.body;
     const userID = $id;
 
@@ -14,10 +17,12 @@ export default async ({ req, res, log, error }) => {
     const dbId = '656af2faa9d4d3352a34';
     const collectionId = 'users';
 
-    // check if user is existed
+    log('checking if user exists');
+    log(`userId, ${userID}`);
     const document = database.getDocument(dbId, collectionId, userID);
-    log('document');
+    log('result: ');
     log(document);
+
     const user = {
       name,
       email,
@@ -25,31 +30,37 @@ export default async ({ req, res, log, error }) => {
       meta: [name, email, phone].join(' '),
       avatar: '',
     };
-    log(req);
-    log(user);
 
     if (!document) {
+      log('user not existed!, creating new one..');
       const createdDoc = await database.createDocument(
         dbId,
         collectionId,
         userID,
         user,
       );
-      log('createddoc');
+      log('created doc: ');
       log(createdDoc);
     } else {
-      const savedDoc = await database.updateDocument(dbId, collectionId, user);
-      log('saveddoc');
-      log(savedDoc);
+      log('user existed, updating..');
+      const updatedDoc = await database.updateDocument(
+        dbId,
+        collectionId,
+        user,
+      );
+      log('updated doc');
+      log(updatedDoc);
     }
   } catch (err) {
-    error(err);
+    error('an error encountered: ');
+    error(JSON.stringify(err, null, 4));
     return res.json({
       message: 'Error while executing function',
       error: err.message,
       rawError: JSON.stringify(err),
     });
   }
+  log('function executed without any error, exiting..');
   return res.json({
     message: 'Success',
     error: null,
