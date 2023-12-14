ef584f24dd1763ac49db6d3a0e43ccd2dad842db
vuggy17 <vuggy17@gmail.com>
2023-12-14 14:06:42 +0700

    feat: friend invitation list ui

--- apps/idle.chat/src/features/profileManagement/ui/components/FriendInvitation.tsx
+++ apps/idle.chat/src/features/profileManagement/ui/components/FriendInvitation.tsx
@@ -1,6 +1,5 @@
 import {
   Alert,
-  Avatar,
   Button,
   Card,
   ConfigProvider,
@@ -11,200 +10,38 @@ import {
   Typography,
   theme,
 } from 'antd';
-import { MouseEvent, Suspense, useMemo, useState } from 'react';
+import { Suspense, useState } from 'react';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { Await, LoaderFunction, useNavigate } from 'react-router-dom';
 import { useLoaderData, defer } from 'react-router-typesafe';
-import GetPendingFriendRequestUseCase from '@idle/chat/features/profileManagement/useCases/getPendingFriendRequests';
-import ModifyFriendRequestUseCase from '@idle/chat/features/profileManagement/useCases/modifyFriendRequest';
 import { Refresh } from 'iconoir-react';
+import { FriendRequestResponseDTO, ID } from '@idle/model';
+import PartialAvatar from '../../../../components/UserCard/PartialAvatar';
+import GetPendingFriendRequestUseCase from '../../useCases/getPendingFriendRequests';
+import ModifyFriendRequestUseCase from '../../useCases/modifyFriendRequest';
+import FriendRequestButton, {
+  FriendRequestActions,
+} from './FriendRequestButton';
 
 dayjs.extend(relativeTime);
 
-type FriendRequest = {
-  id: string;
-  name: string;
-  avatar: string;
-  createdAt: number;
-  isAccepted: boolean;
-};
-
-const mockRequestList: FriendRequest[] = [
-  {
-    id: '01HGD4JGY8992WDQRV6S1RJD8Q',
-    name: 'Bert Gallacher',
-    avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-    isAccepted: true,
-    createdAt: 1676155770,
-  },
-  {
-    id: '01HGD4JGY9842H30NZ5MXZH43Z',
-    name: 'Evvie Hoofe',
-    avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-    isAccepted: false,
-    createdAt: 1675518787,
-  },
-  {
-    id: '01HGD4JGYAMZ3T2CHZH6E7185J',
-    name: 'Romola Walesa',
-    avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-    isAccepted: true,
-    createdAt: 1683131758,
-  },
-  {
-    id: '01HGD4JGYCG72XYPMVA2058H39',
-    name: 'Lynnet McGuiney',
-    avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-    isAccepted: false,
-    createdAt: 1695450104,
-  },
-  {
-    id: '01HGD4JGYD94XZBCH259XC6D9A',
-    name: 'Karlee Allmond',
-    avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-    isAccepted: false,
-    createdAt: 1687655918,
-  },
-  {
-    id: '01HGD4JGYEGDRA30XM8T81SJVJ',
-    name: 'Dulcine Lavington',
-    avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-    isAccepted: true,
-    createdAt: 1677327436,
-  },
-  {
-    id: '01HGD4JGYFPN5RAMPK0N7SYQDT',
-    name: 'Lynnette Rainsbury',
-    avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-    isAccepted: true,
-    createdAt: 1692382620,
-  },
-  // {
-  //   id: '01HGD4JGYGG9Y8YWWZ95F99HF3',
-  //   name: 'Tanya Crecy',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: false,
-  //   createdAt: 1673138330,
-  // },
-  // {
-  //   id: '01HGD4JGYHJQNK0Q2WJ0VC4KBZ',
-  //   name: 'Clemens Bythway',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1681555560,
-  // },
-  // {
-  //   id: '01HGD4JGYJ14AF05XRCPX07GVV',
-  //   name: 'Rosemary Moncreiffe',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1676727133,
-  // },
-  // {
-  //   id: '01HGD4JGYKC4300MBPQ5BNRR6K',
-  //   name: 'Elva Belle',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1686579978,
-  // },
-  // {
-  //   id: '01HGD4JGYM9GDMH30HAN5W9ZY1',
-  //   name: 'Harriot Connaughton',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1683799894,
-  // },
-  // {
-  //   id: '01HGD4JGYNSDAAAGKQC23BXX7P',
-  //   name: 'Olvan Scupham',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: false,
-  //   createdAt: 1671924814,
-  // },
-  // {
-  //   id: '01HGD4JGYPYCA5PVKJTZHBCJB5',
-  //   name: 'Johanna Ricardin',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1700748365,
-  // },
-  // {
-  //   id: '01HGD4JGYQT940YPT7FYYA2DMB',
-  //   name: 'Malchy Karslake',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1684618859,
-  // },
-  // {
-  //   id: '01HGD4JGYRFWCJJVQ8GRPYAYDC',
-  //   name: 'Davina Bowlands',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1676562973,
-  // },
-  // {
-  //   id: '01HGD4JGYS6TPHN7TXRX1YJT1T',
-  //   name: 'Faber Scholz',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1690804252,
-  // },
-  // {
-  //   id: '01HGD4JGYTS7BG1013KVPXBHR4',
-  //   name: 'Farrah Diggles',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: false,
-  //   createdAt: 1683170049,
-  // },
-  // {
-  //   id: '01HGD4JGYVG5MJWRR5786FMJ4V',
-  //   name: 'Barnard Tomaschke',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: false,
-  //   createdAt: 1680163437,
-  // },
-  // {
-  //   id: '01HGD4JGYXAV28T6AM4PJ996H3',
-  //   name: 'Guglielma Digg',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1676676111,
-  // },
-  // {
-  //   id: '01HGD4JGYY7HKQ8BX8N8PT8H7P',
-  //   name: 'Marie-jeanne Burbridge',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1701013730,
-  // },
-  // {
-  //   id: '01HGD4JGYZCDSSY341HK68GCA4',
-  //   name: 'Judah Verillo',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1670873982,
-  // },
-  // {
-  //   id: '01HGD4JGZ01QBM9S5DPGJ9BYAV',
-  //   name: 'Niall Richardeau',
-  //   avatar: 'https://randomuser.me/api/portraits/men/18.jpg',
-  //   isAccepted: true,
-  //   createdAt: 1674188484,
-  // },
-];
+type FriendRequest = FriendRequestResponseDTO;
 
 function FriendInvitation() {
   const data = useLoaderData<typeof Loader>();
   const navigate = useNavigate();
 
   const [errorMsg, setErrorMsg] = useState('');
-  const acceptFriendRequest = async (requestId: string) => {
+  const acceptFriendRequest = async (requestId: ID) => {
     const executor = new ModifyFriendRequestUseCase();
-    await executor.execute({ requestId, action: 'accept' });
+    await executor.execute({
+      requestId,
+      action: 'accept',
+    });
   };
 
-  const deleteFriendRequest = async (requestId: string) => {
+  const deleteFriendRequest = async (requestId: ID) => {
     const executor = new ModifyFriendRequestUseCase();
     await executor.execute({ requestId, action: 'decline' });
   };
@@ -294,13 +131,13 @@ function FriendInvitation() {
                     }}
                     dataSource={requests}
                     itemLayout="horizontal"
-                    renderItem={(item) => (
+                    renderItem={({ sender, updatedAt, ...request }) => (
                       <FriendRequestCard
-                        title={item.name}
-                        avatar={item.avatar}
-                        description={dayjs.unix(item.createdAt).fromNow()}
-                        onAccept={() => acceptFriendRequest(item.id)}
-                        onDelete={() => deleteFriendRequest(item.id)}
+                        title={sender.name}
+                        avatar={sender.avatar}
+                        description={dayjs.unix(updatedAt).fromNow()}
+                        onAccept={() => acceptFriendRequest(request.id)}
+                        onDelete={() => deleteFriendRequest(request.id)}
                         onError={onAcceptOrDeclineError}
                       />
                     )}
@@ -329,85 +166,34 @@ function FriendRequestCard({
   description?: string;
   avatar?: string;
   loading?: boolean;
-  onAccept?: (e: MouseEvent<HTMLButtonElement>) => Promise<void>;
-  onDelete?: (e: MouseEvent<HTMLButtonElement>) => Promise<void>;
+  onAccept?: () => Promise<void>;
+  onDelete?: () => Promise<void>;
   onError?: (error: unknown) => void;
 }) {
   const { token } = useToken();
-
-  const [status, setStatus] = useState<'accepted' | 'deleted' | 'unset'>(
-    'unset',
-  );
   const [hasError, setHasError] = useState(false);
 
-  const onAcceptClick = async (e: MouseEvent<HTMLButtonElement>) => {
+  const handleFriendRequestClicked = async (
+    action: FriendRequestActions,
+    resetState: (error: unknown) => void,
+  ) => {
     try {
-      setStatus('accepted');
-      await onAccept?.(e);
+      switch (action) {
+        case 'ACCEPT':
+          await onAccept?.();
+          break;
+        case 'DECLINE':
+          await onDelete?.();
+          break;
+        default:
+          console.error('Action do not support: ', action);
+      }
     } catch (error) {
-      setStatus('unset');
       setHasError(true);
+      resetState(error);
       onError?.(error);
     }
   };
-  const onDeleteClick = (e: MouseEvent<HTMLButtonElement>) => {
-    try {
-      setStatus('deleted');
-      onDelete?.(e);
-    } catch (error) {
-      setStatus('unset');
-      setHasError(true);
-      onError?.(error);
-    }
-  };
-
-  const footer = useMemo(() => {
-    switch (status) {
-      case 'accepted':
-        return (
-          <Button
-            type="text"
-            block
-            disabled
-            data-testid="friend-invitation__accept--accepted"
-          >
-            Accepted friend request
-          </Button>
-        );
-      case 'deleted':
-        return (
-          <Button
-            type="text"
-            block
-            disabled
-            data-testid="friend-invitation__delete--deleted"
-          >
-            Deleted
-          </Button>
-        );
-      default:
-        return (
-          <Flex className="w-full" gap={8}>
-            <Button
-              type="primary"
-              block
-              onClick={onAcceptClick}
-              data-testid="friend-invitation__accept"
-            >
-              Accept
-            </Button>
-            <Button
-              block
-              onClick={onDeleteClick}
-              data-testid="friend-invitation__delete"
-            >
-              Delete
-            </Button>
-          </Flex>
-        );
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [status]);
 
   const errorStyle: React.CSSProperties = {
     borderColor: token.colorError,
@@ -417,10 +203,10 @@ function FriendRequestCard({
       <Card loading={loading} style={hasError ? errorStyle : undefined}>
         <Flex gap={12} vertical className="w-full">
           <Flex gap={16} align="center">
-            <Avatar
+            <PartialAvatar
               size={58}
               shape="square"
-              src={avatar}
+              src={avatar || title}
               className="shrink-0"
             />
             <div>
@@ -433,7 +219,13 @@ function FriendRequestCard({
             </div>
           </Flex>
           <div className="h-12" />
-          {footer}
+          <div className="text-right">
+            <FriendRequestButton
+              currentUserIsSender={false}
+              initialState="pending"
+              onClick={handleFriendRequestClicked}
+            />
+          </div>
         </Flex>
       </Card>
     </List.Item>
@@ -441,7 +233,7 @@ function FriendRequestCard({
 }
 
 function InvitationListFallback() {
-  const requests = mockRequestList;
+  const requests = Array(6).fill(0);
   return (
     <List<FriendRequest>
       grid={{
@@ -455,8 +247,8 @@ function InvitationListFallback() {
       renderItem={(item) => (
         <FriendRequestCard
           loading
-          title={item.name}
-          avatar={item.avatar}
+          title="title"
+          avatar="avatar"
           description={dayjs.unix(item.createdAt).fromNow()}
         />
       )}
